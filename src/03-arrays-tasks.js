845983468548fda757819ff1d83af5ac0e57db2e
Alexey <alexeyinshakov992@gmail.com>
2024-09-06 07:29:42 +0300

    46 done

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -295,8 +295,9 @@ function propagateItemsByPositionIndex(arr) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  const sortedArr = arr.sort((a, b) => a - b);
+  return sortedArr.slice(-3).reverse();
 }
 
 
@@ -313,8 +314,15 @@ function get3TopItems(/* arr */) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  let count = 0;
+  arr.map((item) => {
+    if (typeof item === 'number' && item > 0) {
+      count += 1;
+    }
+    return undefined;
+  });
+  return count;
 }
 
 /**
