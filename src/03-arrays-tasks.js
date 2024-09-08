73b700544c2c9a4e53c475e4c5fde44a98214c45
Alexey Inshakov <alexeyinshakov992@gmail.com>
2024-09-08 20:46:13 +0300

    57 done

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -653,8 +653,13 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  let current = [...arr];
+  indexes.map((item) => {
+    current = current[item];
+    return undefined;
+  });
+  return current;
 }
 
 
